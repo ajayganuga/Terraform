8a9ef904ab117b51d5d874bc553a31693e8c836f
ajayganuga <ajayganuga@gmail.com>
2026-06-11 21:53:37 +0530

    Terraform Provisioners

---
+++ 10-provisioners/ec2.tf
@@ -0,0 +1,56 @@
+resource "aws_instance" "this" {
+  ami                    = "ami-0220d79f3f480ecf5" 
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.ajaydevops.id]
+
+  tags = {
+    Name = "expense-server"
+  }
+  
+ provisioner "local-exec" {
+    command = "echo ${self.private_ip} > inventory"
+  }
+
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+ provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    when = destroy
+    inline = [
+      "sudo systemctl stop nginx",
+    ]
+  }
+
+}
+resource "aws_security_group" "ajaydevops" {
+  name        = "ajaydevops-sg"
+  description = "Allow TLS traffic and all outbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
